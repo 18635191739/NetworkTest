dc3455d3cf7dc35d5147ffbc6b06cc4edacbbf00
oliver <83003599@qq.com>
2018-08-06 17:00:23 +0800

    2018.8.6 17:00 testSocket

--- src/com/basic/ProjectEntry.java
+++ src/com/basic/ProjectEntry.java
@@ -3,6 +3,7 @@ package com.basic;
 
 import com.JFrames.Example;
 import com.JFrames.MyFrame;
+import com.MySocket.App;
 
 public class ProjectEntry {
     public static void main(String[] args){
@@ -17,8 +18,14 @@ public class ProjectEntry {
         //com.JFrames.Frame1 f =new Frame1();
         //JFrame2 f = new JFrame2();
 
-        MyFrame f = new MyFrame();
+        //MyFrame f = new MyFrame();
         //Example f1 = new Example();
-
+        App sk = new App();
+        if (args[0]=="C"){
+            sk.StartClient();
+        }
+        else {
+            sk.StartServer();
+        }
     }
 }
