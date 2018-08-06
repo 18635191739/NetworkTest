e1974741d59456ec02ac85fb4c12b27056991e4e
oliver <83003599@qq.com>
2018-08-06 10:51:46 +0800

    2018.8.6

--- src/com/basic/ProjectEntry.java
+++ src/com/basic/ProjectEntry.java
@@ -1,7 +1,15 @@
+
 package com.basic;
+
 
 public class ProjectEntry {
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        for(int i = 1; i < args.length; i++) {
+
+            System.out.print(args[i]);
+
+        }
+        System.out.println("Hello World");
+
     }
 }
