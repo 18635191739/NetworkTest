a3cc8da3f30df450cb3f9dd9666065efce8ef36b
oliver <83003599@qq.com>
2018-08-07 18:05:46 +0800

    2018.8.7 18:05 netfinish

--- src/com/basic/ProjectEntry.java
+++ src/com/basic/ProjectEntry.java
@@ -1,15 +1,41 @@
-
 package com.basic;
 
 
+import com.MySocket.App;
+import java.util.Scanner;
+
 public class ProjectEntry {
     public static void main(String[] args){
-        for(int i = 1; i < args.length; i++) {
+//        for(int i = 1; i < args.length; i++) {
+//
+//            System.out.print(args[i]);
+//
+//        }
+//        System.out.println("Hello World");
+//        //Frame1.Myfuc();
+//        Frame2 f = new Frame2();
+        //com.JFrames.Frame1 f =new Frame1();
+        //JFrame2 f = new JFrame2();
 
-            System.out.print(args[i]);
-
+        //MyFrame f = new MyFrame();
+        //Example f1 = new Example();
+        for (String a:args) {
+            System.out.println("args: "+a);
         }
-        System.out.println("Hello World");
+        System.out.println(args.length);
 
+        App sk = new App();
+        if (args.length>0){
+            Scanner s = new Scanner(System.in);
+            String str;
+            System.out.println("请输入您想输入的字符串：");
+            str = s.next();
+            System.out.println("您输入的是：");
+            System.out.println(str);
+            sk.StartClient(str);
+        }
+        else {
+            sk.StartServer();
+        }
     }
 }
